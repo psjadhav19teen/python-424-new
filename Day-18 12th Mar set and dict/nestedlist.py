6f47130b7d8a6dc2009215d2abc3c28c25ec847e
psjadhav19teen <42905297+psjadhav19teen@users.noreply.github.com>
2026-03-12 10:29:15 +0000

    setdemo

--- Day-18 12th Mar set and dict/nestedlist.py
+++ Day-18 12th Mar set and dict/nestedlist.py
@@ -11,14 +11,25 @@ print(students)
 # print("Student id 2 grade is=",students[2][2])
 
 #Find student name and grade by entering student id
+# id=int(input("enter student id to find name and grade="))
+# flag=False  
+# for i in students:
+#     if i[0]==id:
+#         flag=True
+#         print("ID=",i[0])
+#         print("Name=",i[1])
+#         print("Grade=",i[2])
+
+# if flag==False:
+#     print("id not found. Try again!!")
+
+#OR
 id=int(input("enter student id to find name and grade="))
-flag=False  
 for i in students:
     if i[0]==id:
-        flag=True
         print("ID=",i[0])
         print("Name=",i[1])
         print("Grade=",i[2])
-
-if flag==False:
+        break
+else:
     print("id not found. Try again!!")
